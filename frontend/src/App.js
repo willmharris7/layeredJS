c310407dca9326a795086a63f003439c21134cd4
Will Harris <willmharris7@gmail.com>
2021-10-04 04:17:41 -0700

    testing hooks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import ConnectButton from './components/ConnectButton'
+import ReactDefault from './components/ReactDefault'
+import Counter from './components/Counter'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {/* // */}
-        <form action="../../post" method="post" 
-              className="form">
-          <button type="submit">Connected?</button>
-        </form>
-        {/* // */}
+        <ReactDefault/>
+        <ConnectButton/>
+        <Counter/>
       </header>
     </div>
   );
---
+++ frontend/src/components/ConnectButton.js
@@ -0,0 +1,9 @@
+function ConnectButton() {
+    return(
+        <form action="../../post" method="post" className="form">
+            <button type="submit">Connected?</button>
+        </form>
+    )
+}
+
+export default ConnectButton
