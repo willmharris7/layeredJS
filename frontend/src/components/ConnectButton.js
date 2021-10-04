1b1109c7e750063fceede6d98ed84faf9f7feee3
Will Harris <willmharris7@gmail.com>
2021-10-04 10:59:18 -0700

    state working in form field

--- frontend/src/components/ConnectButton.js
+++ frontend/src/components/ConnectButton.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 function ConnectButton() {
     
     const [message, setMessage] = useState("Push to connect")
+    const [data, setData] = useState("")
     
     async function hitServer() {
         const response = await fetch("/api/hello")
@@ -10,10 +11,16 @@ function ConnectButton() {
         console.log(body)
     }
 
+    async function handleSubmit(e) {
+        e.preventDefault()
+        alert(data)
+    }
+
     return(
         <div>
         <button onClick={hitServer}>Connect with express</button>
-        <form action="../../post" method="post" className="form">
+        <form onSubmit={handleSubmit}>
+            <input type="text" onChange={e => setData(e.target.value)}/>
             <button type="submit">{message}</button>
         </form>
         </div>
