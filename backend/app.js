86d4fc9fd611a178c01aac07e26ab0d5353c59b5
Will Harris <willmharris7@gmail.com>
2021-10-04 00:49:35 -0700

    react frontend added

--- backend/app.js
+++ backend/app.js
@@ -18,8 +18,13 @@
 const express = require("express");
 const app = express();
   
-app.get("/", (req, res) => {
-  res.send("Hello World!!!!");
+// app.get("/", (req, res) => {
+//    res.send("Hello World!!!!");
+// });
+
+app.post("/post", (req, res) => {
+  console.log("Connected to React");
+  res.redirect("/");
 });
   
 const PORT = process.env.PORT || 8080;
