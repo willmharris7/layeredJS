b666d4020bc7da44967703e03a01a3b3a63e427d
Will Harris <willmharris7@gmail.com>
2021-10-04 19:13:02 -0700

    invisible div added

--- frontend/src/components/GetDatabasesButton.js
+++ frontend/src/components/GetDatabasesButton.js
@@ -1,7 +1,13 @@
+import React, { useState } from 'react'
+
 function GetDatabasesButton() {
+
+    const [dbList, setDbList] = useState("")
+
     return(
         <div>
             <button>Get database list</button>
+            <div>{dbList}</div>
         </div>
         
     )
