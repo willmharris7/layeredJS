17788af9426a7c06ce6eecb0fb12117c74f77be5
Will Harris <willmharris7@gmail.com>
2021-10-04 19:07:45 -0700

    added get database button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import ConnectButton from './components/ConnectButton'
 import Counter from './components/Counter'
+import GetDatabasesButton from './components/GetDatabasesButton';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <GetDatabasesButton/>
         <ConnectButton/>
         <Counter/>
       </header>
---
+++ frontend/src/components/GetDatabasesButton.js
@@ -0,0 +1,10 @@
+function GetDatabasesButton() {
+    return(
+        <div>
+            <button>Get database list</button>
+        </div>
+        
+    )
+}
+
+export default GetDatabasesButton
