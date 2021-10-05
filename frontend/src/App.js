83d55a31aa31b0e641aaf91f27a5822c5431d08e
Will Harris <willmharris7@gmail.com>
2021-10-04 22:50:27 -0700

    top5 button alert working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import ConnectButton from './components/ConnectButton'
 import Counter from './components/Counter'
 import GetDatabasesButton from './components/GetDatabasesButton';
+import Top5ListingsButton from './components/Top5ListingsButton';
 
 function App() {
   return (
--- frontend/src/components/Top5ListingsButton.js
+++ frontend/src/components/Top5ListingsButton.js
@@ -2,13 +2,16 @@ import React, { useState } from 'react'
 
 function Top5ListingsButton() {
     
+let top5List = "Test"
+
     async function getTop5() {
-        
+        alert("working")
     }
 
     return(
         <div>
             <button onClick={getTop5}>Get 5 AirBnB listings</button>
+            <ul>{top5List}</ul>
         </div>
     )
 }
