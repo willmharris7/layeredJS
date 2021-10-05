09a670992cf568b5a9c87817e4d0a369088b632d
Will Harris <willmharris7@gmail.com>
2021-10-04 19:03:09 -0700

    removed react default text

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import ConnectButton from './components/ConnectButton'
-import ReactDefault from './components/ReactDefault'
 import Counter from './components/Counter'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ReactDefault/>
         <ConnectButton/>
         <Counter/>
       </header>
